9bf0388d055d7a67834de736c2ac2cc0acf16a6e
KiarashYavari <kiarash996@gmail.com>
2022-02-09 12:01:31 +0330

    add retrieve, update and delete api for articles

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArticleListCreateView
+from .views import ArticleListCreateView, ArticleRetrieveUpdateDeleteView
 
 app_name="api"
 urlpatterns = [
-    path('', ArticleListCreateView.as_view(), name="api_list_create")
+    path('', ArticleListCreateView.as_view(), name="api_list_create"),
+    path('<int:pk>', ArticleRetrieveUpdateDeleteView.as_view(), name="api_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 # from django.shortcuts import render
 from .serializers import ArticleListSerializer
 from blog.models import Article
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 class ArticleListCreateView(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleListSerializer
+
+
+class ArticleRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleListSerializer
     
