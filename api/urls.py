2ab72d7650aabcc85d2c7a9de7fe258f2f13e770
KiarashYavari <kiarash996@gmail.com>
2022-02-09 12:11:07 +0330

    add users api to list, create, retrieve, update and delete users

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ArticleListCreateView, ArticleRetrieveUpdateDeleteView
+from .views import (ArticleListCreateView, ArticleRetrieveUpdateDeleteView,
+ UserListCreateView, UserRetrieveUpdateDeleteView)
 
 app_name="api"
 urlpatterns = [
     path('', ArticleListCreateView.as_view(), name="api_list_create"),
     path('<int:pk>', ArticleRetrieveUpdateDeleteView.as_view(), name="api_detail"),
+    path('', UserListCreateView.as_view(), name="api_user_list_create"),
+    path('<int:pk>', UserRetrieveUpdateDeleteView.as_view(), name="api_user_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,27 @@
 # from django.shortcuts import render
-from .serializers import ArticleListSerializer
+from resource import RUSAGE_THREAD
+from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
+from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 class ArticleListCreateView(ListCreateAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleListSerializer
+    serializer_class = ArticleSerializer
 
 
 class ArticleRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleListSerializer
+    serializer_class = ArticleSerializer
+
+
+class UserListCreateView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
