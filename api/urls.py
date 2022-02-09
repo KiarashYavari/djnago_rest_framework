e04b2d939a733e8e98bfac2bbf94bbef5879e325
KiarashYavari <kiarash996@gmail.com>
2022-02-09 12:15:54 +0330

    edit urls for users api

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ app_name="api"
 urlpatterns = [
     path('', ArticleListCreateView.as_view(), name="api_list_create"),
     path('<int:pk>', ArticleRetrieveUpdateDeleteView.as_view(), name="api_detail"),
-    path('', UserListCreateView.as_view(), name="api_user_list_create"),
-    path('<int:pk>', UserRetrieveUpdateDeleteView.as_view(), name="api_user_detail"),
+    path('users/', UserListCreateView.as_view(), name="api_user_list_create"),
+    path('users/<int:pk>', UserRetrieveUpdateDeleteView.as_view(), name="api_user_detail"),
 ]
