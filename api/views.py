2d64a6b515d6afb0df94d913390292a211041fb4
KiarashYavari <kiarash996@gmail.com>
2022-02-25 18:18:20 +0330

    add authenticate permissions and admin permissions

--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
 # from django.shortcuts import render
-from resource import RUSAGE_THREAD
 from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
 from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
 class ArticleListCreateView(ListCreateAPIView):
     queryset = Article.objects.all()
@@ -19,9 +18,10 @@ class ArticleRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
 class UserListCreateView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
 
 
 class UserRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+    permission_classes = (IsAdminUser,)
