339baac04a374e62d4fa7f314f024c0be8dfe436
KiarashYavari <kiarash996@gmail.com>
2022-04-19 15:15:39 +0430

    edit users api permission

--- api/permissions.py
+++ api/permissions.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import SAFE_METHODS
 
 
 class IsStaffOrReadonly(BasePermission):
+    # only staff users can add articles
+    # unstaff users -> readonly
     def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS or
@@ -13,6 +15,7 @@ class IsStaffOrReadonly(BasePermission):
 
 
 class IsAuthorOrReadonly(BasePermission):
+    # staff users can only access their own articles to update or delete them
     def has_object_permission(self, request, view, obj):
         
         if request.method in SAFE_METHODS:
@@ -25,9 +28,13 @@ class IsAuthorOrReadonly(BasePermission):
 
 
 class SuperUserOrReadonly(BasePermission):
+    # only superuser can create, update or delete users
+    # staff -> readonly
+    # auth user, anonym users can not see anything
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS and request.user.is_authenticated or
+            request.method in SAFE_METHODS and 
+            request.user.is_authenticated and request.user.is_staff or
             request.user.is_authenticated and request.user.is_superuser
         )
 
