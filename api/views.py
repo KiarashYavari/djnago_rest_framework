a99ad0cf8eecefa5d9e7b7c1ec6f0c56844addc9
KiarashYavari <kiarash996@gmail.com>
2022-05-09 10:39:49 +0430

    add a revoke token option for api

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 # from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
 from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAuthorOrReadonly, SuperUserOrReadonly
 # Create your views here.
 # add a custom permission for all views list of articles for staff to manipulate for users to read
@@ -29,3 +32,11 @@ class UserRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (SuperUserOrReadonly,)
+
+
+class RevokeToken(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, request):
+        request.auth.delete()
+        return Response(status=204)
