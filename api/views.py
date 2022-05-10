50565df0aad3ed9236116c54080b30729d413403
KiarashYavari <kiarash996@gmail.com>
2022-05-10 15:57:15 +0430

    add a custom view for url that is in email confirm registration

--- api/views.py
+++ api/views.py
@@ -5,8 +5,11 @@ from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
 from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAuthorOrReadonly, SuperUserOrReadonly
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 # Create your views here.
 # add a custom permission for all views list of articles for staff to manipulate for users to read
 # list of users for superuser to manipulate, for staff to read, users no access
@@ -34,9 +37,21 @@ class UserRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     permission_classes = (SuperUserOrReadonly,)
 
 
-class RevokeToken(APIView):
-    permission_classes = (IsAuthenticated,)
+# class RevokeToken(APIView):
+#     permission_classes = (IsAuthenticated,)
 
-    def delete(self, request):
-        request.auth.delete()
-        return Response(status=204)
+#     def delete(self, request):
+#         request.auth.delete()
+#         return Response(status=204)
+
+
+class GoogleLogin(SocialLoginView): # if you want to use Authorization Code Grant, use this
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = 'http://127.0.0.1:8000/api/'
+    client_class = OAuth2Client
+
+
+class EmailConfirmView(APIView):
+    
+    def get(self, request, key):
+        return Response({'detail':"youre registerd successfully."}, status=201)
